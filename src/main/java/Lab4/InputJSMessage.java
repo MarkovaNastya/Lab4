e4469829e2e1c96e9ffdb07ad8fa75e5f46c45ea
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 20:14:05 +0300

    Scripted auto-commit on change (2019-11-22 20:14:05) by gitwatch.sh

--- src/main/java/Lab4/InputJSMessage.java
+++ src/main/java/Lab4/InputJSMessage.java
@@ -6,5 +6,29 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class InputJSMessage {
 
+    private final static String PACKAGE_ID = "packageId";
+    private final static String JS_SCRIPT = "jsScript";
+    private final static String FUNCTION_NAME = "functionName";
+    private final static String TESTS = "tests";
+
+
+
+//    POST
+//    {
+//        "packageId":"11",
+//            "jsScript":"var divideFn = function(a,b) { return a/b} ",
+//            "functionName":"divideFn",
+//            "tests": [
+//        {"testName":"test1",
+//                "expectedResult":"2.0",
+//                "params":[2,1]
+//        },
+//        {"testName":"test2",
+//                "expectedResult":"2.0",
+//                "params":[4,2]
+//        }
+//]
+//    }
+
 
 }
