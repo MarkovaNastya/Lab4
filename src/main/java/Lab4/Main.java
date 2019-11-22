283ff386cedbe63a14060f81ebfa6cea94c2191a
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 19:37:14 +0300

    Scripted auto-commit on change (2019-11-22 19:37:14) by gitwatch.sh

--- src/main/java/Lab4/Main.java
+++ src/main/java/Lab4/Main.java
@@ -14,24 +14,28 @@ import java.util.concurrent.CompletionStage;
 
 public class Main {
 
-//    ActorSystem system = ActorSystem.create("routes");
-//    final Http http = Http.get(system);
-//    final ActorMaterializer materializer = ActorMaterializer.create(system);
-//    MainHttp instance = new MainHttp(system);
-//    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
-//            instance.createRoute(system).flow(system, materializer);
-//    final CompletionStage<ServerBinding> binding = http.bindAndHandle(
-//            routeFlow,
-//            ConnectHttp.toHost("localhost", 8080),
-//            materializer
-//    );
+    public static void main(String[] args) {
+            ActorSystem system = ActorSystem.create("routes");
+    final Http http = Http.get(system);
+    final ActorMaterializer materializer = ActorMaterializer.create(system);
+    MainHttp instance = new MainHttp(system);
+    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+            instance.createRoute(system).flow(system, materializer);
+    final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+            routeFlow,
+            ConnectHttp.toHost("localhost", 8080),
+            materializer
+    );
 
-//System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-//System.in.read();
-//binding
-//        .thenCompose(ServerBinding::unbind)
-//            .
-//
-//    thenAccept(unbound ->system.terminate());
+System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+System.in.read();
+binding
+        .thenCompose(ServerBinding::unbind)
+            .
+
+    thenAccept(unbound ->system.terminate());
+    }
+
+
 
 }
