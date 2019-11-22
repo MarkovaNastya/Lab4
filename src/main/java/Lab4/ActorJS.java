497927ebb27fd295e20dcb1ddb0eea91a642d90b
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 21:01:58 +0300

    Scripted auto-commit on change (2019-11-22 21:01:58) by gitwatch.sh

--- src/main/java/Lab4/ActorJS.java
+++ src/main/java/Lab4/ActorJS.java
@@ -3,12 +3,22 @@ package Lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
-
-
-
-
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class ActorJS extends AbstractActor {
+
+    private String doJSCode (String jscript, String functionName, Object[] params) throws ScriptException, NoSuchMethodException {
+        ScriptEngine engine = new
+                ScriptEngineManager().getEngineByName("nashorn");
+        engine.eval(jscript);
+        Invocable invocable = (Invocable) engine;
+        return invocable.invokeFunction(functionName, params).toString();
+    }
+
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
