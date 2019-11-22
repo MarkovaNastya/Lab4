92a8876d83d0201ec440e7e7d29767fc518bc515
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 19:53:12 +0300

    Scripted auto-commit on change (2019-11-22 19:53:12) by gitwatch.sh

--- src/main/java/Lab4/Actor.java
+++ src/main/java/Lab4/Actor.java
@@ -1,9 +1,16 @@
 package Lab4;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 
 public class Actor extends AbstractActor {
 
+    ActorRef actorJS;
+    ActorRef actorData;
+
+    public Actor() {
+        
+    }
 
 
     @Override
