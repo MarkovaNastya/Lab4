31ab39199a33bb32e54541b0e75c5058fcd452e1
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 20:25:37 +0300

    Scripted auto-commit on change (2019-11-22 20:25:37) by gitwatch.sh

--- src/main/java/Lab4/Test.java
+++ src/main/java/Lab4/Test.java
@@ -1,26 +1,24 @@
 package Lab4;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Test {
 
     private final static String TEST_NAME = "testName";
     private final static String EXPECTED_RESULT = "expectedResult";
     private final static String PARAMS = "params";
 
-//    POST
-//    {
-//        "packageId":"11",
-//            "jsScript":"var divideFn = function(a,b) { return a/b} ",
-//            "functionName":"divideFn",
-//            "tests": [
-//        {"testName":"test1",
-//                "expectedResult":"2.0",
-//                "params":[2,1]
-//        },
-//        {"testName":"test2",
-//                "expectedResult":"2.0",
-//                "params":[4,2]
-//        }
-//]
-//    }
+    private final String testName;
+    private final String expectedResult;
+    private final Object[] params;
 
+
+    @JsonCreator
+
+    public Test(String testName, String expectedResult, Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
 }
