342769def2ace542a1b83745871e73c2413316c5
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 21:34:23 +0300

    Scripted auto-commit on change (2019-11-22 21:34:23) by gitwatch.sh

--- src/main/java/Lab4/ActorData.java
+++ src/main/java/Lab4/ActorData.java
@@ -3,13 +3,19 @@ package Lab4;
 import akka.actor.AbstractActor;
 import akka.japi.pf.ReceiveBuilder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ActorData extends AbstractActor {
+
+    private HashMap<Integer, ArrayList<TestInfo>> storage = new HashMap<>();
+
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(
                         PutDataMessage.class, message ->{
-                            
+
                         }
                 )
                 .build();
