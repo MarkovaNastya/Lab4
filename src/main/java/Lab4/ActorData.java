2a128745a0c1e33996fdea8d1a002f95f4d422f5
MarkovaNastya <nastya_markova4610@mail.ru>
2019-11-22 21:40:58 +0300

    Scripted auto-commit on change (2019-11-22 21:40:58) by gitwatch.sh

--- src/main/java/Lab4/ActorData.java
+++ src/main/java/Lab4/ActorData.java
@@ -15,7 +15,10 @@ public class ActorData extends AbstractActor {
         return ReceiveBuilder.create()
                 .match(
                         PutDataMessage.class, message ->{
-
+                            ArrayList<PutDataMessage> testData = data.containsKey(message.getPackageId()) ?
+                                    data.get(message.getPackageId()) : new ArrayList<>();
+                            testData.add(message);
+                            data.put(message.getPackageId(),testData);
                         }
                 )
                 .build();
